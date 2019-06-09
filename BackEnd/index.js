3e32cfa28417addafca28651d1b3533bca103f07
Alice Alsford <alice.alsford@gmail.com>
2019-06-09 19:26:18 +1200

    added unique id to the new todos

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -16,9 +16,17 @@ app.get("/todos", (req, res) => {
 app.use(bodyParser.json());
 
 app.post("/todos", (req, res) => {
+  var uniqueId = todosDatabase.length + 1;
+  console.log(uniqueId);
+  req.body.id = uniqueId;
+
   console.log(req.body);
   todosDatabase.push(req.body);
   res.send(req.body);
+  //assign todo a unique id
+
+  //save to the database
+  //return it with the unique id
 });
 
 app.listen(3000, () => {
